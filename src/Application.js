747b2d176f4e273bce3150ecef219bd8d2842c8e
sudeptech <sudeptech@gmail.com>
2019-12-15 20:34:12 +0545

    logout issue fixed

--- src/Application.js
+++ src/Application.js
@@ -10,63 +10,80 @@ import Restaurant from "./Restaurant";
 import "./Application.css";
 
 class Application extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentUser: null,
-			loading: true,
-			restaurants: null
-		};
-		this.restaurantRef = database.ref("/restaurants");
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null,
+      loading: true,
+      restaurants: null
+    };
+    this.restaurantRef = database.ref("/restaurants");
+  }
 
-	componentDidMount() {
-		auth.onAuthStateChanged((currentUser) => {
-			if (currentUser) {
-				console.log(`Current User is ${currentUser.displayName}`);
+  componentDidMount() {
+    auth.onAuthStateChanged(currentUser => {
+      if (currentUser) {
+        console.log(`Current User is ${currentUser.displayName}`);
 
-				this.setState({
-					loading: false,
-					currentUser
-				});
-				this.restaurantRef.on("value", (snapShot) => {
-					this.setState({
-						restaurants: snapShot.val()
-					});
-				});
-			} else {
-				this.setState({
-					loading: false,
-				});
-			}
-		});
-	}
-	render() {
-		let cUser;
-		const { currentUser, loading, restaurants } = this.state;
-		if (currentUser && !loading) {
-			cUser = (
-				<div>
-					<NewRestaurant />
-					<Restaurants restaurants={restaurants} user={currentUser} />
-					<CurrentUser user={currentUser} />
-				</div>
-			);
-		} else if (!currentUser) {
-			cUser = loading && <p>Loading</p>;
-			if (!loading) {
-				cUser = <SignIn />;
-			}
-		}
-		return (
-			<div className="Application container">
-				<header className="Application--header">
-					<h1>Lunch Rush</h1>
-					<div>{cUser}</div>
-				</header>
-			</div>
-		);
-	}
+        this.setState({
+          loading: false,
+          currentUser
+        });
+        this.restaurantRef.on("value", snapShot => {
+          this.setState({
+            restaurants: snapShot.val()
+          });
+        });
+      } else {
+        this.setState({
+          loading: false
+        });
+      }
+    });
+  }
+
+  logOut = () => {
+    this.setState({
+      ...this.state,
+      loading: true
+    });
+    auth
+      .signOut()
+      .then(() => {
+        this.setState({
+          currentUser: null,
+          loading: false,
+          restaurants: null
+        });
+      })
+      .catch(error => console.log(error));
+  };
+  render() {
+    let cUser;
+    const { currentUser, loading, restaurants } = this.state;
+    if (currentUser && !loading) {
+      cUser = (
+        <div>
+          <NewRestaurant />
+          <Restaurants restaurants={restaurants} user={currentUser} />
+          <CurrentUser user={currentUser} logOut={this.logOut} />
+        </div>
+      );
+    } else if (!currentUser) {
+      cUser = loading && <p>Loading</p>;
+      if (!loading) {
+        cUser = <SignIn />;
+      }
+    }
+    return (
+      <div className="Application container">
+        <header className="Application--header">
+          <h1>Lunch Rush</h1>
+          <div>{cUser}</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Application;
--- src/CurrentUser.js
+++ src/CurrentUser.js
@@ -3,30 +3,30 @@ import PropTypes from "prop-types";
 import { auth } from "./firebase";
 import "./CurrentUser.css";
 
-const CurrentUser = ({ user }) => {
-	return (
-		<div className="CurrentUser">
-			<img className="CurrentUser--photo" src={user.photoURL} alt={user.displayName} />
-			<div className="CurrentUser--identification">
-				<h3>Welcome {user.displayName}</h3>
-				<p>{user.email}</p>
-				<button
-					onClick={() => auth.signOut().then(console.log("Sign Out")).catch((error) => console.log(error))}
-				>
-					Sign out
-				</button>
-			</div>
-		</div>
-	);
+const CurrentUser = ({ user, logOut }) => {
+  return (
+    <div className="CurrentUser">
+      <img
+        className="CurrentUser--photo"
+        src={user.photoURL}
+        alt={user.displayName}
+      />
+      <div className="CurrentUser--identification">
+        <h3>Welcome {user.displayName}</h3>
+        <p>{user.email}</p>
+        <button onClick={logOut}>Sign out</button>
+      </div>
+    </div>
+  );
 };
 
 CurrentUser.propTypes = {
-	user: PropTypes.shape({
-		displayName: PropTypes.string,
-		email: PropTypes.string.isRequired,
-		photoURL: PropTypes.string,
-		uid: PropTypes.string.isRequired
-	})
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+    photoURL: PropTypes.string,
+    uid: PropTypes.string.isRequired
+  })
 };
 
 export default CurrentUser;
